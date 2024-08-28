0395d9257affcac12ee6c5824a49d3659157a8f6
Vincent <vincentvck8@gmail.com>
2024-08-28 17:20:51 +0200

    registration page

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,17 +5,29 @@ from django.forms import ValidationError
 class RegistrationForm(forms.ModelForm):
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter Password'
+        'placeholder': 'Enter Password'}),
+        error_messages={'required': 'Bitte geben Sie ein Passwort ein.'}
+    )
 
-    }))
-
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'
-
-    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password'}),
+        error_messages={'required': 'Bitte bestätigen Sie Ihr Passwort.'}
+    )
 
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'email', 'password']
+        error_messages = {
+            'first_name': {
+                'required': 'Bitte geben Sie Ihren Vornamen ein.',
+            },
+            'last_name': {
+                'required': 'Bitte geben Sie Ihren Nachnamen ein.',
+            },
+            'email': {
+                'required': 'Bitte geben Sie eine E-Mail-Adresse ein.',
+            },
+        }
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
@@ -24,16 +36,22 @@ class RegistrationForm(forms.ModelForm):
         self.fields['email'].widget.attrs["placeholder"] = 'Email'
         self.fields['password'].widget.attrs["placeholder"] = 'Enter Password'
         self.fields['confirm_password'].widget.attrs["placeholder"] = 'Repeat Password'
-        for field in self.fields:
-            self.fields[field].widget.attrs["class"] = 'form-control'
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Account.objects.filter(email=email).exists():  # Assuming Account model has an email field
+            raise ValidationError(
+                "Dieses E-Mail-Konto existiert bereits. Bitte verwenden Sie eine andere E-Mail-Adresse.")
+        return email
     def clean(self):
         cleaned_data = super(RegistrationForm, self).clean()
         password = cleaned_data.get("password")
         confirm_password = cleaned_data.get("confirm_password")
 
         if password != confirm_password:
-            raise ValidationError('Password does not match')
+            raise ValidationError('Die Passwörter stimmen nicht überein.')
 
 
 class UserForm(forms.ModelForm):
