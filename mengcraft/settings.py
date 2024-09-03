1b184a2632c7ed8906b311013627df1cf9e68950
Vincent <vincentvck8@gmail.com>
2024-09-03 12:05:44 +0200

    nginx config

--- mengcraft/settings.py
+++ mengcraft/settings.py
@@ -200,9 +200,6 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # S3 Custom Domain (optional but recommended)
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-# Limits
-FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
-DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
 
 # Media settings
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
